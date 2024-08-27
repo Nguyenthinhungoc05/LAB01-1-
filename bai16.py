14a17578ec3f5ed538653ca8846566b38302e1cf
Nguyenthinhungoc05 <nguyenthinhungoc200503@gmail.com>
2024-08-27 19:50:58 +0700

    bai16.py

--- bai16.py
+++ bai16.py
@@ -4,7 +4,13 @@ Created on Tue Aug 27 11:24:10 2024
 
 @author: DELL
 """
-
-thoigian= input("nhap vao thoi gian hh:mm:ss : ")
-hh, mm, ss = map(int,thoigian.split (':'))
-print("Doi thoi gian ra giay: ", hh*3600 + mm*60 + ss)
+thoigian= input("nhap vao thoi gian ..h..p..s: ")
+so = ""
+for i in thoigian:
+    if i.isalpha():
+        so += ":"
+    else:
+        so += i
+so_cuoi = so[:-1]
+hh,mm,ss = map(int,so_cuoi.split (':'))
+print(thoigian,"thi doi ra giay bang  ", hh*3600 + mm*60 + ss)

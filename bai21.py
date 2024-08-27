92fa588acb32fe1baa37088c36ec35ce3a4dfee8
Nguyenthinhungoc05 <nguyenthinhungoc200503@gmail.com>
2024-08-27 18:56:39 +0700

    bai21.py

--- bai21.py
+++ bai21.py
@@ -4,28 +4,9 @@ Created on Tue Aug 27 12:00:47 2024
 
 @author: DELL
 """
-
-import math
-a = int(input("Nhap 1 so nguyen bat ky: "))
-if a == 0:
-    print("Khong")
-elif a == 1:
-    print("Mot")
-elif a == 2:
-    print("Hai")
-elif a == 3:
-    print("Ba")
-elif a == 4:
-    print("Bon")
-elif a == 5:
-    print("Nam")
-elif a == 6:
-    print("Sau")
-elif a == 7:
-    print("Bay")
-elif a == 8:
-    print("Tam")
-elif a == 9:
-    print("Chin")
+so = ("Khong", "Mot", "Hai", "Ba", "Bon","Nam","Sau","Bay","Tam","Chin")
+a = int(input("Nhap 1 so nguyen bat ky tu 0 den 9: "))
+if 0 <= a <= 9:
+    print(so[a])
 else:
-    print("Khong doc duoc")    
+    print("Khong doc duoc")

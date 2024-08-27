83b84407101e248de1d5ebd0597ec42d7233218a
Nguyenthinhungoc05 <nguyenthinhungoc200503@gmail.com>
2024-08-27 19:28:12 +0700

    bai22.py

--- bai22.py
+++ bai22.py
@@ -14,9 +14,9 @@ if a==0 and b==0:
     print("Suy ra,phuong trinh co vo so nghiem")
 elif a==0 and b!=0:
     print("Biện luận:")
-    print(f"Vì a = 0 và b = {b} ≠ 0, phương trình trở thành {b} = 0.")
+    print(f"Vì a = 0 và b = {b} != 0, phương trình trở thành {b} = 0.")
     print("Đây là một đẳng thức vô lý.")
     print("Suy ra, phuong trinh vo nghiem")
 else:
     print("Biện luận:")
-    print(f"Vì a = {a} ≠ 0, suy ra phuong trinh co nghiem duy nhat: x =  ",-b/a)
+    print(f"Vì a = {a} != 0, suy ra phuong trinh co nghiem duy nhat: x =  ",-b/a)
